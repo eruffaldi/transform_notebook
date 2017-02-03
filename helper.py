60dabbd8c6ae0b4607d3ad72ea21c7e18cddf7aa
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-02-03 18:58:30 +0100

    initial working

--- helper.py
+++ helper.py
@@ -2,24 +2,21 @@
 import rospy
 import sensor_msgs.msg
 import geometry_msgs.msg
-import ramcip_msgs.msg
 import message_filters
 import numpy as np
-import cv2
 import math
 import os
 import sys
 import yaml
 import pprint
 import tf
-from cv_bridge import CvBridge, CvBridgeError
+import cv2
 import cPickle
 from PyKDL import *
 import tf_conversions
-from ramcip_calib_sssa.srv import *
 import transformations
 import se3d
-
+pi = math.pi
 
 def originrpy2mtx(rpy,origin):
     rot = transformations.quaternion_from_euler(*rpy)
@@ -65,6 +62,8 @@ def mtx2pose(m):
 #origin->SetAttribute("rpy", pose_rpy_str);
 
 def posrvec2mtx(tvec,rvec):
+    tvec = np.array(tvec,dtype=np.float64)
+    rvec = np.array(rvec,dtype=np.float64)
     q = np.identity(4)
     a,b = cv2.Rodrigues(rvec)
     q[0:3,0:3] = a
@@ -76,16 +75,10 @@ def posrvec2mtx(tvec,rvec):
 def posrvec2pose(p, r):
     return mtx2pose(posrvec2mtx(p,r))
 
+def quat2aa(q):
+    #xyzw to wxyz
+    R = transformations.quaternion_matrix(q)  
+    w,J = cv2.Rodrigues(R)
+    n = np.linalg.norm(w)
+    return [180*n/math.pi,w/n]
 
-pos =[-0.04225474 ,0.05194644, 0.00081353]
-quat_xyzw = [0.10640583735010758, 0.008415271547103753, 0.004443887926837299, 0.9942772414385762]
-
-urdf_pos,urdf_rpy =parent_child2urdf(pose2mtx(posequat2pose(pos,quat_xyzw)))
-print "<origin xyz=\"%f %f %f\" rpy=\"%f %f %f\"/>" % (urdf_pos[0],urdf_pos[1],urdf_pos[2],	urdf_rpy[0],urdf_rpy[1],urdf_rpy[2])
-print urdf_rpy[0]*180/math.pi,urdf_rpy[1]*180/math.pi,urdf_rpy[2]*180/math.pi
-
-pos = [0,0,0]
-quat_xyzw = [0.10452846326765346, 0, 0 ,0.9945218953682733]
-urdf_pos,urdf_rpy =parent_child2urdf(pose2mtx(posequat2pose(pos,quat_xyzw)))
-print "<origin xyz=\"%f %f %f\" rpy=\"%f %f %f\"/>" % (urdf_pos[0],urdf_pos[1],urdf_pos[2],	urdf_rpy[0],urdf_rpy[1],urdf_rpy[2])
-print urdf_rpy[0]*180/math.pi,urdf_rpy[1]*180/math.pi,urdf_rpy[2]*180/math.pi
